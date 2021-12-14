d14684a3ada982b385aa1e19eabb6a4a83be0216
Sean Morrissey <semorrissey17@gmail.com>
2021-12-13 19:24:12 -0500

    pushing temporarily so that i can work more

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,69 @@ $(document).ready(function() {
   });
 });
 
+class Clip {
+  constructor() {
+    this.name = "ClipDefault";
+    this.timestamp = [];
+    this.emotion = "EmotionDefault";
+    this.emotionValue = 0;
+    this.people = [];
+    this.thumbnail = document.createElement('img');
+    this.previewGif = document.createElement('img');
+    this.thumbnail.src = "https://i.gyazo.com/0228882f42a760cc23b36c95b78da4f0.png";
+    this.previewGif.src = "https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif";
+  }
+}
+
+class Person {
+  constructor() {
+    this.name = "John Doe";
+    this.topClips = []
+    this.image = document.createElement('img');
+    this.image.src = "https://previews.123rf.com/images/kurhan/kurhan1103/kurhan110300100/9050894-happy-man.jpg";
+  }
+}
+
+var exampleClips = [];
+var examplePeople = [];
+
+
+window.addEventListener('load',
+  function() {
+    examplePeople = [new Person(), new Person(), new Person()]
+
+    for (var i = 0; i < examplePeople.length; i++) {
+
+      for (var j = 0; j < 3; j++) {
+        var temp = new Clip()
+        examplePeople[i].topClips.push(temp)
+        exampleClips.push(temp)
+      }
+    }
+
+    //fill students portion with People
+    for (var i = 0; i < examplePeople.length; i++) {
+      for (var j = 0; j < 3; j++) {
+        document.getElementById("image" + (j + 1).toString()).src = examplePeople[i].image.src
+      }
+      var og = document.getElementById("studentRow")
+      var cln = og.cloneNode(true)
+      document.getElementById("studentBox").appendChild(cln)
+    }
+
+
+    //fills clip section
+    for (var i = 0; i < exampleClips.length; i++) {
+      for (var j = i; j < i + 3; j++) {
+        document.getElementById("thumbnail1").src = exampleClips[j].thumbnail.src
+        document.getElementById("clipName").innerHTML = exampleClips[j].name
+      }
+      var og = document.getElementById("clipBlock")
+      var cln = og.cloneNode(true)
+      document.getElementById("data").appendChild(cln)
+    }
+  }, false);
+
 
 
 function filterClips() {
@@ -31,6 +94,5 @@ function filterClips() {
       item.style.display = "none";
     }
   });
-
 
 }
