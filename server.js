284b96fb7a139be686e4fc06899ddcb157e41004
Sean Morrissey <semorrissey17@gmail.com>
2022-03-16 15:28:09 -0400

    added env fof heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 
 
 const hostname = '127.0.0.1';
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static('public'));
 app.get("/", function(req, res) {
